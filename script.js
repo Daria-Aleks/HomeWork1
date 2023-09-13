db1746b5d95ce6cb76cc80f3ed20ca9849ead496
Daria <daria.aleks1@yandex.ru>
2023-09-13 11:20:32 +0300

    add js

---
+++ script.js
@@ -0,0 +1,57 @@
+//Задание 1
+let a = 10;
+alert (a);
+alert (a + 10);
+//Задание 2
+let FirstIPhone = 2007;
+alert (FirstIPhone);
+//Задание 3
+const Name="Брендан Эйх";
+alert(Name);
+//Задание 4
+let x = 10;
+let y = 2;
+alert (x+y);
+alert (x-y);
+alert (x*y);
+alert (x/y);
+//Задание 5
+let result = (y**5);
+alert(result);
+//Задание 6
+let b = 9;
+let c = 2;
+alert(b%c);
+//Задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+//Задание 8
+let age = prompt ('Сколько вам лет?');
+alert(age);
+//Задание 9.0
+const User={
+    name:'Marti'
+    age: '17'
+    isAdmin: true;
+};
+alert (User.name)
+//Задание 9.1
+User ["city of residence"] = true;
+alert (User["city of residence"]);
+//Задание 9.2
+User.age = "22";
+alert(User.age);
+//Задание 9.3
+delete User["ciy of residence"];
+//Задание 9.4
+let information = prompt('Какую информацию вы хотите узнать о пользователе?');
+alert(User.information);
+//Задание 10
+let UserName = prompt('Назовите ваше имя');
+alert('Привет,' + UserName +'!');
