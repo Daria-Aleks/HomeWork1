6bbca754adca9c8a94124d114be4e5e4a7c8e341
Daria <daria.aleks1@yandex.ru>
2023-09-23 18:12:56 +0300

    исправленный js

--- script.js
+++ script.js
@@ -31,47 +31,25 @@ alert (Number(a) + Number(b));
 //
 let monthNumber = prompt('Введите номер месяца')
 switch (monthNumber) {
+    case '12':
     case '1':
-        console.log(Зима);
-        break;
     case '2':
-        console.log(Зима);
+        console.log("Зима");
         break;
     case '3':
-        console.log(Весна);
-        break;
     case '4':
-        console.log(Весна);
-        break;  
-    case '4':
-        console.log(Весна);
-        break;
     case '5':
-        console.log(Весна);
+        console.log("Весна");
         break;
     case '6':
-        console.log(Весна);
-        break;
     case '7':
-        console.log(Весна);
-        break;
     case '8':
-        console.log(Весна);
+        console.log("Лето");
         break;
     case '9':
-        console.log(Осень);
-        break;
-    case '9':
-        console.log(Осень);
-        break;
     case '10':
-        console.log(Осень);
-        break;
     case '11':
-        console.log(Осень);
-        break;
-    case '12':
-        console.log(Зима);
+        console.log("Осень");
         break;
     default:
     case (monthNumber>=13):
